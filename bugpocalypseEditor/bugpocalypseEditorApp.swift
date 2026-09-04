4bf59aba2ba1b3b5c0a275f2f79f29dcdc3b9c9e
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2026-09-04 08:04:46 +0200

    Initial Commit

---
+++ bugpocalypseEditor/bugpocalypseEditorApp.swift
@@ -0,0 +1,32 @@
+//
+//  bugpocalypseEditorApp.swift
+//  bugpocalypseEditor
+//
+//  Created by Jordi Puigdellívol on 04/09/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct bugpocalypseEditorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ bugpocalypseEditorTests/bugpocalypseEditorTests.swift
@@ -0,0 +1,19 @@
+//
+//  bugpocalypseEditorTests.swift
+//  bugpocalypseEditorTests
+//
+//  Created by Jordi Puigdellívol on 04/09/2026.
+//
+
+import Testing
+@testable import bugpocalypseEditor
+
+struct bugpocalypseEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
